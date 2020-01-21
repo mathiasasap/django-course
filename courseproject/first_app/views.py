154af4e8fe9c5d9a779dc2bff5478e60503e4cb2
mathias <mathias@asapdevelopers.com>
2020-01-21 11:24:54 -0300

    Add table to render a model in a template

--- courseproject/first_app/views.py
+++ courseproject/first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by("date")
     context = {
-        "title": "Welcome to my first app!"
+        "title": "Welcome to my first app!",
+        "access_records": webpages_list,
     }
     return render(request, 'first_app/index.html', context=context)
