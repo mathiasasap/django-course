8c06cbfeb17c950540f476c2f3829d747b32d487
mathias <mathias@asapdevelopers.com>
2020-02-19 11:30:00 -0300

    Update user models and create forms for user

--- courseproject/auth/models.py
+++ courseproject/auth/models.py
@@ -7,3 +7,6 @@ class UserProfileInfo(models.Model):
     # Additional fields
     portfolio_site = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to="profile_pics", blank=True)
+
+    def __str__(self):
+        return self.user.name
